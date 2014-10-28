76ca2de9bf7116bb1ab5c7b2ac034d4eea0a2a9c
Devin Chen <d3vin.chen@gmail.com>
2014-10-29 00:59:03 +0800

    add vimrc

---
+++ .vimrc
@@ -0,0 +1,12 @@
+set smartindent
+set tabstop=4
+set shiftwidth=4
+set expandtab
+
+set hls
+
+"
+" custom syntax hightlight
+"
+au BufRead,BufNewFile *.esp setfiletype javascript
+au BufRead,BufNewFile *.module setfiletype php
---
+++ _setup.sh
@@ -0,0 +1,14 @@
+
+
+#
+# applend this to .bashrc
+#
+## User specific environment and startup programs
+#PATH=$PATH:$HOME/.local/bin:$HOME/bin
+#export PATH
+## use vim to write svn commit log
+#export SVN_EDITOR=vim
+
+
+# vim 
+ln -s /home/chenyang/env/.vimrc /home/chenyang/.vimrc
