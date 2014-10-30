8dcb26dabdaa40c01cfbf62243c85114553068c4
Devin Chen <d3vin.chen@gmail.com>
2014-10-30 20:38:19 +0800

    mount opt dir

--- setup.sh
+++ setup.sh
@@ -18,3 +18,6 @@ ln -s /home/chenyang/env/bin/ /home/chenyang/bin
 
 # git config
 ln -s /home/chenyang/env/.gitconfig /home/chenyang/.gitconfig
+
+# mount opt
+sshfs -o reconnect,allow_other,default_permissions -o uid=1000 -o gid=1000 -o IdentityFile=/home/chenyang/.ssh/3dly.net.key -o StrictHostKeyChecking=no chenyang@192.168.2.21:/home/chenyang/cloud/opt /opt
