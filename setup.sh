b40ba1d8d330857cfe0b99cadfc85cf183ee77df
Devin Chen <d3vin.chen@gmail.com>
2014-11-03 12:33:54 +0800

    workstation needs java runtime env

--- setup.sh
+++ setup.sh
@@ -25,3 +25,7 @@ ln -s /home/chenyang/env/.gitconfig /home/chenyang/.gitconfig
 
 # mount opt
 sshfs -o reconnect,allow_other,default_permissions -o uid=1000 -o gid=1000 -o IdentityFile=/home/chenyang/.ssh/3dly.net.key -o StrictHostKeyChecking=no chenyang@192.168.2.21:/home/chenyang/cloud/opt /opt
+
+# hudson slave needs java runtime env
+# apache sling need java runtime env (java7 may not work)
+sudo apt-get install openjdk-6-jdk
