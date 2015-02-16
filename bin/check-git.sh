828de59d6d934a46cd88355bd552e8462a97ed15
Devin Chen <d3vin.chen@gmail.com>
2015-02-16 16:22:35 +0800

    check no commit and noty me

---
+++ bin/check-git.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+function noty()
+{
+  export DISPLAY=:0.0 && export XAUTHORITY=/home/chenyang/.Xauthority && /usr/bin/notify-send "$1" "$2" -i ~/Downloads/swiper-autoplay.png
+}
+
+function check()
+{
+  files="`git ls-files --other --directory --exclude-standard`"
+  git ls-files --other --directory --exclude-standard | sed q1 || noty "noty" "uncommit changes in git /home/chenyang/env\nfile list:\n$files"
+}
+
+function check1()
+{
+  cd "$1" && check && cd -
+}
+function check2()
+{
+  cd "/home/chenyang/source/$1" && check && cd -
+}
+
+# check not commit
+
+check1 "/home/chenyang/env"
+check2 "performance"
+check2 "xxd3vin.github.io"
+
+# check not up2date
