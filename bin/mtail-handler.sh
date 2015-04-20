80e72ad844b20dc307d609249312c540adc30016
Devin Chen <d3vin.chen@gmail.com>
2015-04-20 18:51:05 +0800

    fix syntax error

--- bin/mtail-handler.sh
+++ bin/mtail-handler.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#LOG175="/home/chenyang/Mount/0.175sftp/var/log/npm.log"
 LOG176="/home/chenyang/Mount/0.176sftp/var/log/npm.log"
 LOG177="/home/chenyang/Mount/0.177sftp/var/log/npm.log"
 LOG178="/home/chenyang/Mount/0.178sftp/var/log/npm.log"
@@ -9,7 +8,6 @@ LOG180="/home/chenyang/Mount/0.180sftp/var/log/npm.log"
 LOG181="/home/chenyang/Mount/0.181sftp/var/log/npm.log"
 
 multitail -s 2 \
-#  -ci green  $LOG175 \
   -ci red    $LOG176 \
   -ci red    $LOG177 \
   -ci red    $LOG178 \
