8106f46d60e0d430a9267c5755949fe858fa1dac
Devin Chen <d3vin.chen@gmail.com>
2015-04-29 17:04:07 +0800

    Show all render log

---
+++ bin/less-handler.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+LOG174="/home/chenyang/Mount/0.174sftp/var/log/npm.log"
+LOG175="/home/chenyang/Mount/0.175sftp/var/log/npm.log"
+LOG176="/home/chenyang/Mount/0.176sftp/var/log/npm.log"
+LOG177="/home/chenyang/Mount/0.177sftp/var/log/npm.log"
+LOG178="/home/chenyang/Mount/0.178sftp/var/log/npm.log"
+LOG179="/home/chenyang/Mount/0.179sftp/var/log/npm.log"
+LOG180="/home/chenyang/Mount/0.180sftp/var/log/npm.log"
+LOG181="/home/chenyang/Mount/0.181sftp/var/log/npm.log"
+
+cat $LOG176 $LOG177 $LOG178 $LOG179 $LOG180 |less
