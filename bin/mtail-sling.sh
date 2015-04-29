650ea407f817ee944744614309483d74f0390ddf
Devin Chen <d3vin.chen@gmail.com>
2015-04-29 17:03:47 +0800

    Show all sling node log

--- bin/mtail-sling.sh
+++ bin/mtail-sling.sh
@@ -2,7 +2,9 @@
 
 LOG60="/home/chenyang/Mount/0.60sftp/var/log/glue.log"
 LOG62="/home/chenyang/Mount/0.62sftp/var/log/glue.log"
+LOG64="/home/chenyang/Mount/0.64sftp/var/log/glue.log"
 
-multitail -s 2 \
-  -ci green  $LOG60 \
-  -ci yellow $LOG62
+multitail -s 3 \
+  -ci red    $LOG60 \
+  -ci green  $LOG62 \
+  -ci yellow $LOG64
