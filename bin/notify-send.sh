79fe484c1c0c5f83e0c6b3242a1d49eb993421d6
Devin Chen <d3vin.chen@gmail.com>
2015-06-18 18:47:41 +0800

    ubuntu notification

---
+++ bin/notify-send.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+title="$1"
+content="$2"
+
+export DISPLAY=:0.0 && export XAUTHORITY=/home/chenyang/.Xauthority && /usr/bin/notify-send "$title" "$content" -i ~/Downloads/swiper-autoplay.png
