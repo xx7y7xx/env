8bdc02a4bcc19b8ca0345476cd481eacb73b181b
Devin Chen <d3vin.chen@gmail.com>
2015-06-19 19:24:29 +0800

    Sleep a while for jenkins job

--- bin/notify-send.sh
+++ bin/notify-send.sh
@@ -4,3 +4,5 @@ title="$1"
 content="$2"
 
 export DISPLAY=:0.0 && export XAUTHORITY=/home/chenyang/.Xauthority && /usr/bin/notify-send "$title" "$content" -i ~/Downloads/swiper-autoplay.png
+# sleep for jenkins job
+sleep 10
