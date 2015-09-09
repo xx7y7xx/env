ec7b45e232fd20c03ccefae662bdc094b4da3612
Devin Chen <d3vin.chen@gmail.com>
2015-09-09 13:29:35 +0800

    remove unused bo

--- bin/do.sh
+++ bin/do.sh
@@ -1,1 +1,1 @@
-autossh -i .ssh/3dly.net.key root@128.199.66.129 -D 127.0.0.1:9999
+autossh -i ~/.ssh/3dly.net.key root@128.199.66.129 -D 127.0.0.1:9999
--- bin/do2.sh
+++ bin/do2.sh
@@ -1,1 +1,1 @@
-autossh -i .ssh/3dly.net.key root@128.199.90.7 -D 127.0.0.1:9998
+autossh -i ~/.ssh/3dly.net.key root@128.199.90.7 -D 127.0.0.1:9998
