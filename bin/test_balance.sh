46acb6b322ed348578d42e9740aa15cdf3a5ca97
Devin Chen <d3vin.chen@gmail.com>
2015-09-18 15:53:53 +0800

    print timestamp

--- bin/test_balance.sh
+++ bin/test_balance.sh
@@ -2,6 +2,7 @@
 
 while :
 do
+  printf "[`date`] " | tee -a /tmp/a.txt
   curl -s http://192.168.0.61:8080/sysop.test.jcr_status | sed '/^\s*$/d' | tee -a /tmp/a.txt
-  sleep 0.1
+  sleep 0.5
 done
