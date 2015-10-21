e86f488b104874b3e5277192e42b92dc23f25221
Devin Chen <d3vin.chen@gmail.com>
2015-10-21 18:48:48 +0800

    fix username error

--- bin/rebootrouter.sh
+++ bin/rebootrouter.sh
@@ -1,1 +1,2 @@
-curl "http://admin:admin@192.168.1.1/userRpm/SysRebootRpm.htt=%D6%D8%C6%F4%C2%B7%D3%C9%C6%F7"
+url="http://192.168.1.1/userRpm/SysRebootRpm.htm?Reboot=%D6%D8%C6%F4%C2%B7%D3%C9%C6%F7";
+curl -u admin:admin "$url"
