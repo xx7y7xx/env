4e4a620fe0a29e35c8545b3e44a99ad07ebc8c77
Devin Chen <d3vin.chen@gmail.com>
2015-10-26 15:05:41 +0800

    Add script name

--- bin/test_balance.sh
+++ bin/test_balance.sh
@@ -3,6 +3,6 @@
 while :
 do
   printf "[`date`] " | tee -a /tmp/a.txt
-  curl -s http://192.168.0.61:8080/sysop.test.jcr_status | sed '/^\s*$/d' | tee -a /tmp/a.txt
+  curl -s http://192.168.0.61:8080/sysop.test.jcr_status?from=test_balance.sh | sed '/^\s*$/d' | tee -a /tmp/a.txt
   sleep 0.5
 done
