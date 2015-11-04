92f1cdd9dd8dd408b43638e5ca9367bdf2af598b
Devin Chen <d3vin.chen@gmail.com>
2015-11-04 19:14:24 +0800

    Auto switch to 192.168.1.220

--- bin/rebootrouter.sh
+++ bin/rebootrouter.sh
@@ -1,2 +1,5 @@
 url="http://192.168.1.1/userRpm/SysRebootRpm.htm?Reboot=%D6%D8%C6%F4%C2%B7%D3%C9%C6%F7";
+
+nmcli c up id 192.168.1.220
 curl -u admin:admin "$url" -s > /dev/null
+nmcli c up id 192.168.2.220
