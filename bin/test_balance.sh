ad5356bfd2971cf32d20bb18ab1aababd229ed09
Devin Chen <d3vin.chen@gmail.com>
2015-11-20 15:09:47 +0800

    Check curl exit code

--- bin/test_balance.sh
+++ bin/test_balance.sh
@@ -3,6 +3,10 @@
 while :
 do
   printf "[`date`] " | tee -a /tmp/a.txt
-  curl -s http://192.168.0.61:8080/sysop.test.jcr_status?from=test_balance.sh | sed '/^\s*$/d' | tee -a /tmp/a.txt
+  #curl --max-time 10 -s http://192.168.0.61:8080/sysop.test.jcr_status?from=test_balance.sh | sed '/^\s*$/d' | tee -a /tmp/a.txt
+  curl --max-time 10 -s http://192.168.0.61:8080/sysop.test.jcr_status?from=test_balance.sh
+  if [ $? -ne 0 ]; then
+    echo "curl($?)"
+  fi
   sleep 0.5
 done
