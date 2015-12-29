7e16570865a8cf761f5233875f3de83b002b9da0
Devin Chen <d3vin.chen@gmail.com>
2015-12-29 16:02:53 +0800

    No need sleep for Jenkins job.

--- bin/notify-send.sh
+++ bin/notify-send.sh
@@ -5,4 +5,4 @@ content="$2"
 
 export DISPLAY=:0.0 && export XAUTHORITY=/home/chenyang/.Xauthority && /usr/bin/notify-send "$title" "$content" -i ~/Downloads/swiper-autoplay.png
 # sleep for jenkins job
-sleep 10
+#sleep 10
