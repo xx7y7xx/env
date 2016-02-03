e11c7f587c8d594dbe30c49c756498a06e0de452
Devin Chen <d3vin.chen@gmail.com>
2016-02-03 19:14:36 +0800

    Download all logs to merge to a big one.

--- bin/less-sling.sh
+++ bin/less-sling.sh
@@ -1,8 +1,44 @@
 #!/bin/bash
 
-LOG60="/home/chenyang/Mount/0.60sftp/var/log/glue.log"
-LOG62="/home/chenyang/Mount/0.62sftp/var/log/glue.log"
-LOG64="/home/chenyang/Mount/0.64sftp/var/log/glue.log"
+# Optional param.
+log_path="$1"
 
-#cat $LOG60 $LOG62 $LOG64 |less
-cat $LOG60 $LOG62 |less
+if [ "X$1" = "Xhelp" ]; then
+  echo "log type:"
+  echo "  - /var/log/glue.log"
+  echo "  - /tmp/glue_fangan_to_wware.log"
+  exit 0
+fi
+
+ROOT_DIR="/home/chenyang/Mount"
+BIGLOG="/tmp/sling_big_log.txt"
+
+ip_list=(
+"0.62"
+"0.64"
+"0.65"
+"0.66"
+"0.67"
+"0.68"
+"0.70"
+"0.71"
+)
+
+# log type, default is glue.log
+if [ -z $log_path ]; then
+  log_path="/var/log/glue.log"
+fi
+
+echo "" > $BIGLOG
+
+for ip in "${ip_list[@]}"
+do
+  logfile="$ROOT_DIR/${ip}sftp/$log_path"
+  if [ -s "$logfile" ];then
+    cat $logfile >> $BIGLOG
+  fi
+done
+
+echo "All logs saved to $BIGLOG"
+
+$cmd
