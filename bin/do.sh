0ef7de0e8bb1f3b2bd74d671302aa0941967ed4d
Devin Chen <d3vin.chen@gmail.com>
2016-08-10 12:50:42 +0800

    change server ip

--- bin/do.sh
+++ bin/do.sh
@@ -3,19 +3,17 @@
 # ip list
 
 sgp="128.199.90.7"
-sfo1="159.203.243.15"
+nyc1="192.241.129.192"
 
 case "$1" in
   sgp) ip=$sgp
   ;;
-  sfo1) ip=$sfo1
+  nyc1) ip=$nyc1
   ;;
-  nyc1) ip="192.241.129.192"
-  ;;
-  *) ip=$sgp
+  *) ip=$nyc1
   ;;
 esac
 
 echo $ip
 
-autossh root@$ip -D 0.0.0.0:9998
+autossh root@$ip #-D 0.0.0.0:9998
