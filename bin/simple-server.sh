8561c885d1dfe43134ea28bff666a85c315fb46f
Devin Chen <d3vin.chen@gmail.com>
2017-08-29 09:33:20 +0800

    dir param for simple-server command

--- bin/simple-server.sh
+++ bin/simple-server.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 chromium-browser http://localhost:8000
-python -m SimpleHTTPServer
+
+if [ "X$1" = "X" ]; then
+  python -m SimpleHTTPServer
+else
+  cd $1
+  python -m SimpleHTTPServer
+  cd -
+fi
+
