752eecc534bf763bd8ed51f0d6e1a09dc9c15077
Devin Chen <d3vin.chen@gmail.com>
2017-09-26 16:30:38 +0800

    save to npm package.json

--- bin/tnpm.sh
+++ bin/tnpm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -n $1 ]; then
-  npm install $1 --registry=https://registry.npm.taobao.org
+  npm install $1 -S --registry=https://registry.npm.taobao.org
 else
   npm install --registry=https://registry.npm.taobao.org
 fi
